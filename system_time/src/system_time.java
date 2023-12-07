5a98df63d27e47b0a07156a3ae61afb2a84a2836
viettoan1020 <viettoann102003@gmail.com>
2023-12-07 14:55:43 +0700

    thuchanh1

---
+++ system_time/src/system_time.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class system_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
